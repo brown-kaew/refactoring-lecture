c1dd4d44271729d17ddcb506e3f8f9898d3f0f13
Kaew Tibkham <kaewtibkham@gmail.com>
2022-12-30 13:32:55 +0700

    Add FizzBuzz 12 should get Fizz

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -6,7 +6,7 @@ func FizzBuzz(num int) string {
 	if num == 5 || num == 10 {
 		return "Buzz"
 	}
-	if num == 3 || num == 6 || num == 9 {
+	if num == 3 || num == 6 || num == 9 || num == 12 {
 		return "Fizz"
 	}
 
